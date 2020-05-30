512583b4bb346695e3284ca42fc0c87b3d18ebd5
gheraibia <gheraibia@hotmail.com>
2020-05-30 11:08:48 +0200

    Add files via upload
    inciption became register and I add two new classes

---
+++ php/MngDb.php
@@ -0,0 +1,37 @@
+<?php
+require("../config.php");
+class MngDb{
+	private $_mysqli;
+	
+	//constructor
+	public function __construct() {
+		$this->_mysqli= mysqli_connect(MYSQL_IP, MYSQL_USER, MYSQL_PASSWORD, MYSQL_TABLE);
+		if (mysqli_connect_errno($this->_mysqli)) {
+			echo "Error Mysqli : " . mysqli_connect_error();
+		}
+	}
+	
+	//getter
+	public function mysqli() {
+		$this->_mysqli;
+	}
+	
+	//method
+	public function execRequest($request, $formError){
+		$result=mysqli_query($this->_mysqli, $request);
+		if(!$result && ERROR_USER > 0)
+		{
+			die("Mysql Error :".mysqli_error($this->_mysqli));
+		}
+		else if(!$result && ERROR_USER == 0)
+		{
+			die('<div class="formstatuserror">Error : '.$formError.'</div>');
+		}
+		return $result;
+	}
+	
+	public function close(){
+		mysqli_close($this->_mysqli);
+	}
+}
+?>
